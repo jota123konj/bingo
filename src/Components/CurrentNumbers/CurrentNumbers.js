a845ca735a67713ea241cff9a9a90fb0f414ec0f
m8o <mate.zelenika@student.fsr.ba>
2021-05-01 19:00:56 +0200

    initial

--- src/Components/CurrentNumbers/CurrentNumbers.js
+++ src/Components/CurrentNumbers/CurrentNumbers.js
@@ -4,69 +4,25 @@ import Number from './Number';
 
 const CurrentNumbers = (props) => {
     if(props.roundNumbers !== undefined){
-        const [ numbersState, setNumbersState] = useState({
-            index: 0,
-            firstFive: [],
-            remainingNumbers: []
-        });
-        let endTime = props.roundEndTime;
+        let numbers = props.roundNumbers;
+        let firstFive = [null];
+        let remainingNumbers = [null];
+
         
-        function setFunction(numberParam){
-            if(numbersState.index < 5) {
-                
-            setNumbersState({firstFive: numbersState.firstFive.push(parseInt(numberParam, 10)),
-                            index: numbersState.index++});
-            console.log(numbersState.index);
-                        }
-            else {
-                setNumbersState({remainingNumbers: numbersState.remainingNumbers.push(parseInt(numberParam, 10)),
-                    index: numbersState.index++});
-                    console.log(numbersState.remainingNumbers);
+        numbers.forEach((element, i) => {
+            if(i < 5){
+                firstFive[i] = <Number>{element}</Number>
+            }else{
+                remainingNumbers[i-5] = <Number>{element}</Number>
             }
-            // splitArrayFun();
-        }
-        
-     
-        // function splitArrayFun() {
-        //     let index = numbersState.numbers.length -1;
-        //     console.log(index);
-        
-        //     if(index<5){
-        //         setNumbersState({...firstFive[index]:numbersState.numbers[index]});
-        //     }else{
-        //         remainingNumbers[index -5] = numbersState.numbers[index];
-
-        //     }
-            
-        // }
-        let firstFiveFunc;
-        if(numbersState.firstFive === []){
-        firstFiveFunc = numbersState.firstFive.map((number) => {
-            return <Number>{number}</Number>
-          });
-        }
-        let remainingNumbersFunc = numbersState.remainingNumbers.map((number) => {
-            return <Number>{number}</Number>
-        })
-        // console.log(firstFiveFunc);
-
-
-
-        if(numbersState.index < 35){
-            setInterval(() => {
-                setFunction(props.roundNumbers[numbersState.index])
-            }, 1000);
-        }else{
-            
-        }
-
+        });
 
         return(
             <div className = 'currentNumbers'>
                 <div className = "firstFive"> 
-                    {firstFiveFunc}
+                    {firstFive}
                 </div>
-                {remainingNumbersFunc}
+                {remainingNumbers}
             </div>
     
         );
@@ -76,3 +32,59 @@ const CurrentNumbers = (props) => {
 }
 
 export default CurrentNumbers;
+// const [ numbersState, setNumbersState] = useState({
+    //     index: 0,
+    //     firstFive: [],
+    //     remainingNumbers: []
+    // });
+    // let endTime = props.roundEndTime;
+    
+    // function setFunction(numberParam){
+    //     if(numbersState.index < 5) {
+            
+    //     setNumbersState({...numbersState,firstFive: numbersState.firstFive.push(parseInt(numberParam, 10)),
+    //                     index: numbersState.index++});
+    //     console.log(numbersState.firstFive);
+    //                 }
+    //     else {
+    //         setNumbersState({...numbersState,remainingNumbers: numbersState.remainingNumbers.push(parseInt(numberParam, 10)),
+    //             index: numbersState.index++});
+    //             console.log(numbersState.remainingNumbers);
+    //     }
+    //     // splitArrayFun();
+    // }
+    
+ 
+    // // function splitArrayFun() {
+    // //     let index = numbersState.numbers.length -1;
+    // //     console.log(index);
+    
+    // //     if(index<5){
+    // //         setNumbersState({...firstFive[index]:numbersState.numbers[index]});
+    // //     }else{
+    // //         remainingNumbers[index -5] = numbersState.numbers[index];
+
+    // //     }
+        
+    // // }
+    // let firstFiveFunc;
+    // if(numbersState.firstFive === []){
+    // firstFiveFunc = numbersState.firstFive.map((number) => {
+    //     return <Number>{number}</Number>
+    //   });
+    // }
+    // let remainingNumbersFunc = numbersState.remainingNumbers.map((number) => {
+    //     return <Number>{number}</Number>
+    // })
+    // // console.log(firstFiveFunc);
+
+
+
+    // if(numbersState.index < 35){
+    //     setInterval(() => {
+    //         setFunction(props.roundNumbers[numbersState.index])
+    //     }, 1000);
+    // }else{
+        
+    // }
+
