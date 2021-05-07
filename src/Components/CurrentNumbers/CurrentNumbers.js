e41a797ed99579af2beee9f362020ffd26f2bae5
toneymar11 <tonykopilas11@gmail.com>
2021-05-07 13:33:43 +0200

    ne radi git

--- src/Components/CurrentNumbers/CurrentNumbers.js
+++ src/Components/CurrentNumbers/CurrentNumbers.js
@@ -1,67 +1,70 @@
-import React from 'react';
-import './CurrentNumbers.css';
-import Number from './Number';
-import Quote from './Quote';
+import React from "react";
+import "./CurrentNumbers.css";
+import Number from "./Number";
+import Quote from "./Quote";
+import "./Number.css";
 
 const CurrentNumbers = (props) => {
-    
-    if(props.roundNumbers !== undefined){
+  if (props.roundNumbers !== undefined) {
+    let firstTenQuotes = [
+      "1000",
+      "7500",
+      "5000",
+      "2500",
+      "1000",
+      "500",
+      "300",
+      "200",
+      "150",
+      "100",
+    ];
+    let secondTenQuotes = [
+      "90",
+      "80",
+      "70",
+      "60",
+      "50",
+      "40",
+      "30",
+      "25",
+      "20",
+      "15",
+    ];
+    let lastTenQuotes = ["10", "9", "8", "7", "6", "5", "4", "3", "2", "1"];
 
-        let firstTenQuotes = ["10000","7500","5000","2500","1000","500","300","200","150","100"];
-        let secondTenQuotes = ["90","80","70","60","50","40","30","25","20","15"];
-        let lastTenQuotes = ["10","9","8","7","6","5","4","3","2","1"];
+    let firstFive = [null];
+    let firstTen = [null];
+    let secondTen = [null];
+    let lastTen = [null];
+    firstTenQuotes.forEach((element, i) => {
+      firstTenQuotes[i] = <Quote key={i}>{firstTenQuotes[i]}</Quote>;
+      secondTenQuotes[i] = <Quote key={i}>{secondTenQuotes[i]}</Quote>;
+      lastTenQuotes[i] = <Quote key={i}>{lastTenQuotes[i]}</Quote>;
+    });
+    props.roundNumbers.forEach((element, i) => {
+      if (i < 5) {
+        firstFive[i] = <Number key={i}>{element}</Number>;
+      } else if (i < 15) {
+        firstTen[i - 5] = <Number key={i}>{element}</Number>;
+      } else if (i < 25) {
+        secondTen[i - 15] = <Number key={i}>{element}</Number>;
+      } else {
+        lastTen[i - 25] = <Number key={i}>{element}</Number>;
+      }
+    });
 
-        let firstFive = [null];
-        let firstTen = [null];
-        let secondTen = [null];
-        let lastTen = [null];
-        firstTenQuotes.forEach((element, i) => {
-            firstTenQuotes[i] = <Quote key = {i}>{firstTenQuotes[i]}</Quote>;
-            secondTenQuotes[i] = <Quote key = {i}>{secondTenQuotes[i]}</Quote>;
-            lastTenQuotes[i] = <Quote key = {i}>{lastTenQuotes[i]}</Quote>
-        });
-        props.roundNumbers.forEach((element, i) => {
-            if(i < 5){
-                firstFive[i] = <Number key = {i}>{element}</Number>
-            }else if(i<15){
-                firstTen[i-5] = <Number key = {i}>{element}</Number>
-            }else if (i<25) {
-                secondTen[i-15] = <Number key = {i}>{element}</Number>                
-            } else {
-                lastTen[i-25] = <Number key = {i}>{element}</Number>
-            }
-        });
-
-        return(
-            <div className = 'currentNumbers'>
-                <div className = "firstFive"> 
-                    {firstFive}
-                </div>
-                <div className="remainingNumbers">
-                    {firstTen}
-                </div>
-                <div className="firstQuotes">
-                    {firstTenQuotes}
-                </div>                
-                <div className="remainingNumbers">
-                    {secondTen}
-                </div>
-                <div className="secondQuotes">
-                    {secondTenQuotes}
-                </div>                
-                <div className="remainingNumbers">
-                    {lastTen}
-                </div>
-                <div className="lastQuotes">
-                    {lastTenQuotes}
-                </div>                
-            </div>
-            
-    
-        );
-    }
-    else return null;
-    
-}
+    return (
+      <div className="currentNumbers">
+        <div className="firstFive">{firstFive}</div>
+        <div className="remainingNumbers">{firstTen}</div>
+        <div className="firstQuotes">{firstTenQuotes}</div>
+        <div className="remainingNumbers">{secondTen}</div>
+        <div className="secondQuotes">{secondTenQuotes}</div>
+        <div className="remainingNumbers">{lastTen}</div>
+        <div className="lastQuotes">{lastTenQuotes}</div>
+      </div>
+    );
+  } else return null;
+};
 
 export default CurrentNumbers;
--- src/Components/CurrentNumbers/Number.js
+++ src/Components/CurrentNumbers/Number.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import './Number.css';
+import React from "react";
+import "./Number.css";
 
 const Number = (props) => {
-    const Color = (number) => {
-        switch (number%8){
-            case 1:
-                return "redColor";
-            case 2:
-                return "greenColor";
-            case 3:
-                return "blueColor";
-            case 4:
-                return "purpleColor";
-            case 5:
-                return "brownColor";
-            case 6:
-                return "yellowColor";
-            case 7:
-                return "orangeColor";
-            default:
-                return "blackColor";
-        }
+  const Color = (number) => {
+    switch (number % 8) {
+      case 1:
+        return "redColor";
+      case 2:
+        return "greenColor";
+      case 3:
+        return "blueColor";
+      case 4:
+        return "purpleColor";
+      case 5:
+        return "brownColor";
+      case 6:
+        return "yellowColor";
+      case 7:
+        return "orangeColor";
+      default:
+        return "blackColor";
     }
-    return(
-        
-            <div className = 'number'>
-                <div className = {Color(props.children)}>{props.children}</div>
-            </div>
+  };
+ 
 
-    );
-    
-}
+  return (
+    <div className="number num">
+      <div className={Color(props.children)}>{props.children}</div>
+    </div>
+  );
+};
 export default Number;
